a2e2a8a672a1a0da14b0ca6b58e4fc103b9a998c
Paul Kern <paul.kern@insa-strasbourg.fr>
2021-04-27 17:02:21 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Paul Kern | Home page</title>
+
+	<!-- Bootstrap -->
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+
+	<link rel="stylesheet" type="text/css" href="style.css">
+
+	<link rel="icon" href="icon.ico">
+
+</head>
+<body>
+
+<section id="presentation">
+	<div class="container">
+		<div class="row">
+			<div class="centered col-sm-8 offset-2">
+				<h1 class="margetop45">Paul Kern</h1>
+				<h3 class="margetop45">Etudiant en Génie Electrique à l'INSA Strasbourg</h3>
+				<p class="margetop45">Je m'appelle Paul Kern, j'ai 21 ans et je suis actuellement en dernière année d'études à l'INSA Strasbourg en <span class="bleu">Génie Electrique</span>, spécialité <span class="bleu italique">Systèmes Embarqués & Internet des Objets</span>.
+				<br> Ayant bientôt terminé mon stage de fin d'études chez <a href="https://www.vox.care">Vox.Care</a>, je suis à la recherche de mon premier emploi dans le domaine de l'électronique embarquée, si possible dans les technologies spatiales.</p>
+				<p>Hors des cours, je suis passionné par les mathématiques, la programmation et les jeux vidéos : mes différents projets, tant personnels que scolaires, peuvent donner des exemples de mes passions.</p>
+				<a href="files/CV_KERN_PAUL.pdf" class="btn btn-primary" class="margetop45">Télécharger mon CV</a>
+			</div>
+		</div>
+	</div>
+</section>
+
+<section id="education">
+	<div class="container">
+		formation
+	</div>
+</section>
+
+<section id="experiences">
+	<div class="container">
+		expériences
+	</div>
+</section>
+
+<section id="skills">
+	<div class="container">
+		compétences
+	</div>
+</section>
+
+<section id="projects">
+	<div class="container">
+		projets
+	</div>
+</section>
+
+<section id="contests">
+	<div class="container">
+		concours
+	</div>
+</section>
+
+</body>
+</html>
