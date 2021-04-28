e648236da66872d74c50647dad4ed7772f45ec44
Paul Kern <paul.kern@vox.care>
2021-04-28 16:19:11 +0200

    Added presentation and formation

--- index.php
+++ index.php
@@ -1,12 +1,12 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Paul Kern | Home page</title>
+	<title>Paul Kern | Page d'accueil</title>
 
 	<!-- Bootstrap -->
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
 
-	<link rel="stylesheet" type="text/css" href="style.css">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
 
 	<link rel="icon" href="icon.ico">
 
@@ -16,13 +16,14 @@
 <section id="presentation">
 	<div class="container">
 		<div class="row">
-			<div class="centered col-sm-8 offset-2">
+			<div class="text-center col-sm-8 offset-2">
 				<h1 class="margetop45">Paul Kern</h1>
 				<h3 class="margetop45">Etudiant en Génie Electrique à l'INSA Strasbourg</h3>
-				<p class="margetop45">Je m'appelle Paul Kern, j'ai 21 ans et je suis actuellement en dernière année d'études à l'INSA Strasbourg en <span class="bleu">Génie Electrique</span>, spécialité <span class="bleu italique">Systèmes Embarqués & Internet des Objets</span>.
-				<br> Ayant bientôt terminé mon stage de fin d'études chez <a href="https://www.vox.care">Vox.Care</a>, je suis à la recherche de mon premier emploi dans le domaine de l'électronique embarquée, si possible dans les technologies spatiales.</p>
-				<p>Hors des cours, je suis passionné par les mathématiques, la programmation et les jeux vidéos : mes différents projets, tant personnels que scolaires, peuvent donner des exemples de mes passions.</p>
-				<a href="files/CV_KERN_PAUL.pdf" class="btn btn-primary" class="margetop45">Télécharger mon CV</a>
+				<img src="img/photo_profil.jpg" class="margetop45" width="200" height="200" alt="Photo profil">
+				<p class="margetop45">Je m'appelle Paul Kern, j'ai 21 ans et je suis actuellement en dernière année d'études en <span class="bleu">Génie Electrique</span> à l'INSA Strasbourg, spécialité <span class="bleu italique">Systèmes Embarqués & Internet des Objets</span>.
+				<br> Ayant bientôt terminé mon stage de fin d'études chez <a href="https://www.vox.care">Vox Care</a>, je suis à la recherche de mon premier emploi dans le domaine de l'électronique embarquée, si possible dans les technologies spatiales.</p>
+				<p>Hors des cours, je suis passionné par les mathématiques, la programmation et les jeux vidéos : mes différents projets, tant personnels que scolaires, peuvent donner des exemples de ce que je sais faire.</p>
+				<a href="files/CV_KERN_PAUL.pdf" class="btn btn-primary margetop45 margebot45">Télécharger mon CV</a>
 			</div>
 		</div>
 	</div>
@@ -30,7 +31,38 @@
 
 <section id="education">
 	<div class="container">
-		formation
+		<div class="row">
+			<div class="text-center col-sm-8 offset-2">
+				<h2>Formation</h2>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-sm-2 text-center center-image-vert">
+				<img src="img/logo_insa.svg" class="img-thumbnail" alt="Logo INSA Strasbourg">
+			</div>
+			<div class="col-sm-8">
+				<p style="margin-top: 10px"><span class="bleu">2016 - 2021</span> — INSA Strasbourg — Diplôme d'Ingénieur Génie Electrique</p>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-sm-6 offset-2 gris">
+				Cours : <ul>
+					<li>Electronique numérique</li>
+					<li>Electronique analogique</li>
+					<li>Traitement du signal</li>
+					<li>Systèmes Temps Réel</li>
+					<li>Big Data et Machine Learning</li>
+				</ul>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-sm-2 text-center center-image-vert">
+				<img src="img/logo_saint_michel.png" class="img-thumbnail" alt="Logo Saint-Michel">
+			</div>
+			<div class="col-sm-10">
+				<p style="margin-top: 10px"><span class="bleu">2016</span> — Lycée Saint-Michel des Batignolles, Paris — Baccalauréat Général S - Spé Math - Mention Très Bien</p>
+			</div>
+		</div>
 	</div>
 </section>
 
