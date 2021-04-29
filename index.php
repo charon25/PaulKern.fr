9506437efacfb3091ab8c7e030f32a3ac69da2a0
Paul Kern <paul.kern@vox.care>
2021-04-29 11:55:50 +0200

    Now correctly responsive

--- index.php
+++ index.php
@@ -9,6 +9,7 @@
 	<link rel="stylesheet" type="text/css" href="css/style.css">
 
 	<link rel="icon" href="icon.ico">
+	<meta content='maximum-scale=1.0, initial-scale=1.0, width=device-width' name='viewport'>
 
 </head>
 <body>
@@ -16,14 +17,14 @@
 <section id="presentation">
 	<div class="container">
 		<div class="row">
-			<div class="text-center col-sm-8 offset-2">
-				<h1 class="margetop45">Paul Kern</h1>
-				<h3 class="margetop45">Etudiant en Génie Electrique à l'INSA Strasbourg</h3>
+			<div class="col-sm-12 text-center">
+				<h1 class="margetop45 fw-bold">Paul Kern</h1>
+				<h3 class="">Etudiant en Génie Electrique à l'INSA Strasbourg</h3>
 				<img src="img/photo_profil.jpg" class="margetop45" width="200" height="200" alt="Photo profil">
-				<p class="margetop45">Je m'appelle Paul Kern, j'ai 21 ans et je suis actuellement en dernière année d'études en <span class="bleu">Génie Electrique</span> à l'INSA Strasbourg, spécialité <span class="bleu italique">Systèmes Embarqués & Internet des Objets</span>.
-				<br> Ayant bientôt terminé mon stage de fin d'études chez <a href="https://www.vox.care">Vox Care</a>, je suis à la recherche de mon premier emploi dans le domaine de l'électronique embarquée, si possible dans les technologies spatiales.</p>
-				<p>Hors des cours, je suis passionné par les mathématiques, la programmation et les jeux vidéos : mes différents projets, tant personnels que scolaires, peuvent donner des exemples de ce que je sais faire.</p>
-				<a href="files/CV_KERN_PAUL.pdf" class="btn btn-primary margetop45 margebot45">Télécharger mon CV</a>
+				<p class="margetop45 justif">Je m'appelle Paul Kern, j'ai 21 ans et je suis actuellement en dernière année d'études en <span class="bleu">Génie Electrique</span> à l'INSA Strasbourg, spécialité <span class="bleu italique">Systèmes Embarqués & Internet des Objets</span>.</p>
+				<p class="justif">Ayant bientôt terminé mon stage de fin d'études chez <a href="https://www.vox.care">Vox Care</a>, je suis à la recherche de mon premier emploi dans le domaine de l'électronique embarquée, si possible dans les technologies spatiales.</p><br>
+				<p class="justif">Hors des cours, je suis passionné par les mathématiques, la programmation et les jeux vidéos : mes différents projets, tant personnels que scolaires, peuvent donner des exemples de ce que je sais faire.</p>
+				<a href="files/CV_KERN_PAUL.pdf" class="btn btn-primary margetop25 margebot45">Télécharger mon CV</a>
 			</div>
 		</div>
 	</div>
@@ -32,6 +33,25 @@
 <section id="education">
 	<div class="container">
 		<div class="row">
+			<div class="text-center col-sm-12">
+				<h2 class="margebot45">Formation</h2>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-sm-6 text-center">
+				<img src="img/logo_insa.svg" class="img-thumbnail-mod-height" alt="Logo INSA Strasbourg" height=60>
+				<p class="margetop25"><span class="bleu">2016 - 2021</span><br>
+				<span class="fw-bold">INSA Strasbourg</span><br>
+				Diplôme d'Ingénieur en Génie Electrique</p>
+			</div>
+			<div class="col-sm-6 text-center">
+				<img src="img/logo_saint_michel.png" class="img-thumbnail-mod-height" alt="Logo Saint-Michel" height=60>
+				<p class="margetop25"><span class="bleu">2016</span><br>
+				<span class="fw-bold">Lycée Saint-Michel des Batignolles</span><br>
+				Baccalauréat Général S - Spé Maths - Mention Très Bien</p>
+			</div>
+		</div>
+		<!--<div class="row">
 			<div class="text-center col-sm-8 offset-2">
 				<h2>Formation</h2>
 			</div>
@@ -45,14 +65,20 @@
 			</div>
 		</div>
 		<div class="row">
-			<div class="col-sm-6 offset-2 gris">
-				Cours : <ul>
-					<li>Electronique numérique</li>
-					<li>Electronique analogique</li>
-					<li>Traitement du signal</li>
-					<li>Systèmes Temps Réel</li>
-					<li>Big Data et Machine Learning</li>
-				</ul>
+			<div class="col-sm-2"></div>
+			<div class="col-sm-3 gris">
+				<p>
+					Electronique numérique<br>
+					Electronique analogique<br>
+					Programmation FPGA en VHDL
+				</p>
+			</div>
+			<div class="col-sm-3 gris">
+				<p>
+					Traitement du signal<br>
+					Systèmes Temps Réel<br>
+					Big Data & Machine Learning
+				</p>
 			</div>
 		</div>
 		<div class="row">
@@ -62,7 +88,7 @@
 			<div class="col-sm-10">
 				<p style="margin-top: 10px"><span class="bleu">2016</span> — Lycée Saint-Michel des Batignolles, Paris — Baccalauréat Général S - Spé Math - Mention Très Bien</p>
 			</div>
-		</div>
+		</div>-->
 	</div>
 </section>
 
