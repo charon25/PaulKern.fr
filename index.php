8d2cf2935aa9fb1a58a604edc143c5593c31ad68
charon25 <paul.kern@vox.care>
2021-04-30 10:40:10 +0200

    Better design

--- index.php
+++ index.php
@@ -6,6 +6,10 @@
 	<!-- Bootstrap -->
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
 
+	<!-- Polices -->
+	<link href="https://fonts.googleapis.com/css?family=Roboto:400,400i" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Lora:400,400i" rel="stylesheet">
+
 	<link rel="stylesheet" type="text/css" href="css/style.css">
 
 	<link rel="icon" href="icon.ico">
@@ -14,20 +18,37 @@
 </head>
 <body>
 
-<section id="presentation">
+<section id="presentation-first">
 	<div class="container">
 		<div class="row">
 			<div class="col-sm-12 text-center">
-				<h1 class="margetop45 fw-bold">Paul Kern</h1>
-				<h3 class="">Etudiant en Génie Electrique à l'INSA Strasbourg</h3>
-				<img src="img/photo_profil.jpg" class="margetop45" width="200" height="200" alt="Photo profil">
-				<p class="margetop45 justif">Je m'appelle Paul Kern, j'ai 21 ans et je suis actuellement en dernière année d'études en <span class="bleu">Génie Electrique</span> à l'INSA Strasbourg, spécialité <span class="bleu italique">Systèmes Embarqués & Internet des Objets</span>.</p>
-				<p class="justif">Ayant bientôt terminé mon stage de fin d'études chez <a href="https://www.vox.care">Vox Care</a>, je suis à la recherche de mon premier emploi dans le domaine de l'électronique embarquée, si possible dans les technologies spatiales.</p><br>
-				<p class="justif">Hors des cours, je suis passionné par les mathématiques, la programmation et les jeux vidéos : mes différents projets, tant personnels que scolaires, peuvent donner des exemples de ce que je sais faire.</p>
+				<h1 class="margetop45 fw-bold titre-principal">Paul Kern</h1>
+				<h3 class="titre-principal">Etudiant en Génie Electrique à l'INSA Strasbourg</h3>
 				<a href="files/CV_KERN_PAUL.pdf" class="btn btn-primary margetop25 margebot45">Télécharger mon CV</a>
 			</div>
 		</div>
 	</div>
+</section>
+	<div class="container">
+		<div class="row">
+			<div class="col-sm-12 text-center">
+				<h2 class="titre">À propos de moi</h2>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-sm-3 text-center">
+				<img src="img/photo_profil.jpg" width="200" height="200" class="img-thumbnail-mod-height margebot25" alt="Photo profil">
+			</div>
+			<div class="col-sm-9">
+				<p class="justif">Je m'appelle Paul Kern, j'ai 21 ans et je suis actuellement en dernière année d'études en <span class="bleu">Génie Electrique</span> à l'INSA Strasbourg, spécialité <span class="bleu italique">Systèmes Embarqués & Internet des Objets</span>.</p>
+				<p class="justif">Ayant bientôt terminé mon stage de fin d'études chez <a href="https://www.vox.care">Vox Care</a>, je suis à la recherche de mon premier emploi dans le domaine de l'électronique embarquée, si possible dans les technologies spatiales.</p><br>
+				<p class="justif">Hors des cours, je suis passionné par les mathématiques, la programmation et les jeux vidéos : mes différents projets, tant personnels que scolaires, peuvent donner des exemples de ce que je sais faire.</p>
+			</div>
+			<div class="margebot25"></div>
+		</div>
+	</div>
+<section>
+	
 </section>
 
 <section id="education">
@@ -43,7 +64,7 @@
 				<p class="margetop25"><span class="bleu-big">2016 - 2021</span><br>
 				<span class="fw-bold">INSA Strasbourg</span><br>
 				Diplôme d'Ingénieur en Génie Electrique</p>
-			</div>
+			</div><hr class="hr">
 			<div class="col-sm-6 text-center bordure-left">
 				<img src="img/logo_saint_michel.png" class="img-thumbnail-mod-height" alt="Logo Saint-Michel" height=60>
 				<p class="margetop25"><span class="bleu-big">2016</span><br>
@@ -70,14 +91,14 @@
 				Stage de fin d'études</p>
 				<p class="txt-gauche gris">Amélioration du produit principal : Serena, un assistant vocal à destinations des établissements de santé.<br>
 				Plusieurs sujets abordés : reconnaissance vocale, débruitage d'un signal, détection Bluetooth...</p>
-			</div>
+			</div><hr class="hr">
 			<div class="text-center col-sm-4 bordure-left bordure-right">
 				<img src="img/logo_kpu.png" class="img-thumbnail-mod-height" alt="Logo KPU" height=60>
 				<p class="margetop25"><span class="bleu-big">Juin - Août 2019</span><br>
 				<span class="fw-bold">Korea Polytechnic University</span><br>
 				Stage de découverte culturelle</p>
 				<p class="txt-gauche gris">Self-learning en Arduino et programmation Web.</p>
-			</div>
+			</div><hr class="hr">
 			<div class="text-center col-sm-4 bordure-left">
 				<img src="img/logo_kpu.png" class="img-thumbnail-mod-height" alt="Logo Cordon" height=60>
 				<p class="margetop25"><span class="bleu-big">Juillet 2017</span><br>
@@ -104,7 +125,7 @@
 				<span class="fw-bold">MiniJam 76</span><br>
 				Jeu réalisé de A à Z en 72h en Python<br>
 				<a href="https://charon25.itch.io/spalt">Page du jeu sur itch.io</a></p>
-			</div>
+			</div><hr class="hr">
 			<div class="text-center col-sm-6 bordure-left">
 				<a href="https://www.sauas.fr/"><img src="img/img_sauas.png" class="img-thumbnail-mod-height" alt="Logo SAUAS" height=150></a>
 				<p class="margetop25"><span class="bleu-big">Juin - Octobre 2019 & Septembre - Novembre 2020</span><br>
@@ -129,20 +150,20 @@
 			</div>
 		</div>
 		<div class="row">
-			<div class="text-center col-sm-4">
-				<img src="img/flags/flag_fr.svg" alt="Français" height="65" class="img-thumbnail-mod-height">
+			<div class="text-center col-sm-3">
+				<img src="img/flags/flag_fr.svg" alt="Français" height="65" class="img-thumbnail-mod-height logo">
 				<p class="bleu">Langue maternelle</p>
 			</div>
-			<div class="text-center col-sm-4">
-				<img src="img/flags/flag_en.svg" alt="Anglais" height="65" class="img-thumbnail-mod-height">
+			<div class="text-center col-sm-3">
+				<img src="img/flags/flag_en.svg" alt="Anglais" height="65" class="img-thumbnail-mod-height logo">
 				<p class="bleu">Courant - C1 (TOEIC : 990/990)</p>
 			</div>
-			<div class="text-center col-sm-4">
-				<img src="img/flags/flag_de.svg" alt="Allemand" height="65" class="img-thumbnail-mod-height">
+			<div class="text-center col-sm-3">
+				<img src="img/flags/flag_de.svg" alt="Allemand" height="65" class="img-thumbnail-mod-height logo">
 				<p class="bleu">Bases - A2</p>
 			</div>
-			<div class="text-center col-sm-4">
-				<img src="img/flags/flag_kr.svg" alt="Coréen" height="65" class="img-thumbnail-mod-height">
+			<div class="text-center col-sm-3">
+				<img src="img/flags/flag_kr.svg" alt="Coréen" height="65" class="img-thumbnail-mod-height logo">
 				<p class="bleu">Notions - A1</p>
 			</div>
 		</div>
@@ -158,11 +179,11 @@
 		</div>
 		<div class="row">
 			<div class="text-center col-sm-2">
-				<img src="img/logo_python.svg" alt="Python" height="65">
+				<img src="img/logo_python.svg" alt="Python" height="65" class="logo">
 				<p class="bleu">Python 3</p>
 			</div>
 			<div class="text-center col-sm-2">
-				<img src="img/logo_vb.svg" alt="VB.NET" height="65">
+				<img src="img/logo_vb.svg" alt="VB.NET" height="65" class="logo">
 				<p class="bleu">Visual Basic .NET</p>
 			</div>
 			<div class="margebot25"></div>
@@ -174,11 +195,11 @@
 		</div>
 		<div class="row">
 			<div class="text-center col-sm-2">
-				<img src="img/logo_cpp.svg" alt="C++" height="65">
+				<img src="img/logo_cpp.svg" alt="C++" height="65" class="logo">
 				<p class="bleu">C++</p>
 			</div>
 			<div class="text-center col-sm-2">
-				<img src="img/logo_vhdl.png" alt="VHDL" height="65">
+				<img src="img/logo_vhdl.png" alt="VHDL" height="65" class="logo">
 				<p class="bleu">VHDL</p>
 			</div>
 			<div class="margebot25"></div>
