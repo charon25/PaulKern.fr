44f4ba2bf24d49bfbc2aba9d3dec7f3f8d6a9974
charon25 <paul.kern@vox.care>
2021-04-30 16:12:47 +0200

    Language classification

--- footer.php
+++ footer.php
@@ -1,3 +1,4 @@
+<?php  ?>
 <footer id="footer">
 	<p><a href="LICENSE">License MIT</a> : Copyright (c) 2021 Paul Kern (<a href="mailto:paul.kern.fr@gmail.com">paul.kern.fr@gmail.com</a>)</p>
 </footer>
--- header.php
+++ header.php
@@ -1,3 +1,4 @@
+<?php  ?>
 <header>
 
 	<div id="menu" class="container">
