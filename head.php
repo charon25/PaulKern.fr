605fcd480172c1e11d0863e1a4d2b13bc798dd93
Paul Kern <paul.kern@insa-strasbourg.fr>
2021-04-30 22:11:38 +0200

    <head> in its one file

---
+++ head.php
@@ -0,0 +1,25 @@
+<?php function print_head($title) { ?>
+
+<head>
+	<title>Paul Kern | <?php echo $title; ?></title>
+
+	<!-- Bootstrap -->
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+
+	<!-- Polices -->
+	<link href="https://fonts.googleapis.com/css?family=Roboto:400,400i" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Roboto+Mono:400,400i" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Lora:400,400i" rel="stylesheet">
+
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+
+	<link rel="icon" href="icon.ico">
+	<meta content='maximum-scale=1.0, initial-scale=1.0, width=device-width' name='viewport'>
+
+	<!-- Icônes -->
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css"
+		integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+
+</head>
+
+<?php } ?>
--- index.php
+++ index.php
@@ -1,26 +1,6 @@
 <!DOCTYPE html>
 <html>
-<head>
-	<title>Paul Kern | Page d'accueil</title>
-
-	<!-- Bootstrap -->
-	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
-
-	<!-- Polices -->
-	<link href="https://fonts.googleapis.com/css?family=Roboto:400,400i" rel="stylesheet">
-	<link href="https://fonts.googleapis.com/css?family=Roboto+Mono:400,400i" rel="stylesheet">
-	<link href="https://fonts.googleapis.com/css?family=Lora:400,400i" rel="stylesheet">
-
-	<link rel="stylesheet" type="text/css" href="css/style.css">
-
-	<link rel="icon" href="icon.ico">
-	<meta content='maximum-scale=1.0, initial-scale=1.0, width=device-width' name='viewport'>
-
-	<!-- Icônes -->
-	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css"
-		integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
-
-</head>
+<?php include('head.php'); print_head("Page d'accueil"); ?>
 <body>
 
 
