7a8c9724ed9fa2b2d678a9043556e623ecd85bdb
charon25 <paul.kern@vox.care>
2021-04-30 15:20:40 +0200

    Separate page experiences pro

---
+++ exp_pro.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Paul Kern | Expériences professionnelles</title>
+
+	<!-- Bootstrap -->
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+
+	<!-- Polices -->
+	<link href="https://fonts.googleapis.com/css?family=Roboto:400,400i" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Roboto+Mono:400,400i" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Lora:400,400i" rel="stylesheet">
+
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+
+	<link rel="icon" href="icon.ico">
+	<meta content='maximum-scale=1.0, initial-scale=1.0, width=device-width' name='viewport'>
+
+	<!-- Icônes -->
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css"
+		integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+
+</head>
+
+<body>
+
+<?php include('header.php'); ?>
+
+<section id="presentation-first" class="first-section">
+	<div class="container">
+		<div class="row">
+			<div class="col-sm-12 text-center">
+				<h1 class="margetop45 fw-bold titre-principal">Expériences professionnelles</h1>
+				<div class="margebot45"></div>
+			</div>
+		</div>
+	</div>
+</section>
+
+<section class="even-section">
+	<div class="container">
+		<div class="row">
+			<div class="margetop25"></div>
+			<div class="col-sm-3 text-center">
+				<img src="img/logo_voxcare.svg" class="img-thumbnail-mod-height margebot25 miniature" alt="Photo profil">
+			</div>
+			<div class="col-sm-9">
+				<p class="bleu-big">Février - Août 2021</p>
+				<p class="fw-bold">Vox Care</p>
+				<p>Stage de fin d'études : travail sur le produit Serena, une enceinte vocale pour établissement de santé</p>
+				<div class="row gris">
+					<div class="col-sm-5">
+						<p>Reconnaissance vocale<br>Détection Bluetooth<br>Interphone WebRTC</p>
+					</div>
+					<div class="col-sm-5">
+						<p>Traitement du signal audio<br>Speech To Text<br>Docker</p>
+					</div>
+				</div>
+			</div>
+			<div class="margebot25"></div>
+		</div>
+	</div>
+</section>
+
+<section class="odd-section">
+	<div class="container">
+		<div class="row">
+			<div class="margetop25"></div>
+			<div class="col-sm-3 text-center">
+				<img src="img/logo_kpu.png" class="img-thumbnail-mod-height margebot25 miniature" alt="Photo profil">
+			</div>
+			<div class="col-sm-9">
+				<p class="bleu-big">Juin - Août 2019</p>
+				<p class="fw-bold">KPU</p>
+				<p>Stage de fin d'études : travail sur le produit Serena, une enceinte vocale pour établissement de santé</p>
+				<div class="row gris">
+					<div class="col-sm-5">
+						<p>Reconnaissance vocale<br>Détection Bluetooth<br>Interphone WebRTC</p>
+					</div>
+					<div class="col-sm-5">
+						<p>Traitement du signal audio<br>Speech To Text<br>Docker</p>
+					</div>
+				</div>
+			</div>
+			<div class="margebot25"></div>
+		</div>
+	</div>
+</section>
+
+
+
+<?php include('footer.php'); ?>
+
+</body>
+</html>
