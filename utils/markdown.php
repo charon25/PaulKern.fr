db90590b315d596c860f0a37c6e672ff30574da7
charon25 <paul.kern@vox.care>
2021-05-03 14:25:52 +0200

    First pseudo-markdown draft

---
+++ utils/markdown.php
@@ -0,0 +1,16 @@
+<?php 
+
+function get_html($string) {
+	$string = preg_replace('/\*(.*)\*/s', '<span class="md-bold">\1</span>', $string);
+	$string = preg_replace('/_(.*)_/s', '<span class="md-italics">\1</span>', $string);
+	$string = preg_replace('/\$(.*)\$/s', '<span class="md-highlight">\1</span>', $string);
+
+	return $string;
+}
+
+
+
+
+
+
+ ?>
