58491032a724284f564a59138df17f9219f5a1cb
charon25 <paul.kern@vox.care>
2021-05-04 13:56:12 +0200

    Exp pro page with PHP

--- exp_pro.php
+++ exp_pro.php
@@ -1,3 +1,15 @@
+<?php 
+
+require('utils/bdd.php');
+require('utils/markdown.php');
+require('utils/constants.php');
+require('utils/functions.php');
+
+
+$experiences_pro_data = get_db_data_from_key($bdd, 'pro', -1);
+
+ ?>
+
 <!DOCTYPE html>
 <html>
 <?php include('utils/head.php'); print_head(array('title' => "Expériences professionnelles")); ?>
@@ -17,57 +29,24 @@
 	</div>
 </section>
 
-<section class="even-section">
-	<div class="container">
-		<div class="row">
-			<div class="margetop25"></div>
-			<div class="col-sm-3 text-center">
-				<img src="img/logo_voxcare.svg" class="img-thumbnail-mod-height margebot25 miniature" alt="Photo profil">
-			</div>
-			<div class="col-sm-9">
-				<p class="bleu-big">Février - Août 2021</p>
-				<p class="fw-bold">Vox Care</p>
-				<p>Stage de fin d'études : travail sur le produit Serena, une enceinte vocale pour établissement de santé</p>
-				<div class="row gris">
-					<div class="col-sm-5">
-						<p>Reconnaissance vocale<br>Détection Bluetooth<br>Interphone WebRTC</p>
-					</div>
-					<div class="col-sm-5">
-						<p>Traitement du signal audio<br>Speech To Text<br>Docker</p>
-					</div>
-				</div>
-			</div>
-			<div class="margebot25"></div>
-		</div>
-	</div>
-</section>
+<?php 
 
-<section class="odd-section">
-	<div class="container">
-		<div class="row">
-			<div class="margetop25"></div>
-			<div class="col-sm-3 text-center">
-				<img src="img/logo_kpu.png" class="img-thumbnail-mod-height margebot25 miniature" alt="Photo profil">
-			</div>
-			<div class="col-sm-9">
-				<p class="bleu-big">Juin - Août 2019</p>
-				<p class="fw-bold">KPU</p>
-				<p>Stage de fin d'études : travail sur le produit Serena, une enceinte vocale pour établissement de santé</p>
-				<div class="row gris">
-					<div class="col-sm-5">
-						<p>Reconnaissance vocale<br>Détection Bluetooth<br>Interphone WebRTC</p>
-					</div>
-					<div class="col-sm-5">
-						<p>Traitement du signal audio<br>Speech To Text<br>Docker</p>
-					</div>
-				</div>
-			</div>
-			<div class="margebot25"></div>
-		</div>
-	</div>
-</section>
+foreach ($experiences_pro_data as $key => $experience) {
+	if ($key%2 == 0) {echo '<section class="even-section">';}
+	else {echo '<section class="odd-section">';}
 
+	echo '<div class="container"><div class="row"><div class="margetop25"></div>';
+	echo '<div class="col-sm-3 text-center"><img src="' . $experience[$EXP_A_MAIN_IMG] . '" class="img-thumbnail-mod-height margebot25 miniature" alt="Image ' . $experience[$EXP_A_NAME] . '"></div>';
+	echo '<div class="col-sm-9">';
+	echo '<p><span class="bleu-big">' . $experience[$EXP_A_DATE_TXT] . '</span><br>';
+	echo '<span class="fw-bold">' . $experience[$EXP_A_NAME] . '</span><br>';
+	echo $experience[$EXP_A_TITLE] . '</p>';
+	echo '<div class="gris">' . markdown_to_html($experience[$EXP_A_DESC]) . '</div>';
+	echo '</div><div class="margebot25"></div></div></div></section>';
 
+}
+
+ ?>
 
 <?php include('utils/footer.php'); print_footer();?>
 
--- utils/markdown.php
+++ utils/markdown.php
@@ -1,25 +1,29 @@
 <?php 
 
 function markdown_to_html($string) {
+	// Bold
 	$string = preg_replace('/\*([^*]*)\*/s', '<span class="md-bold">\1</span>', $string);
+	// Italics
 	$string = preg_replace('/_([^.]*)_/s', '<span class="md-italics">\1</span>', $string);
+	// Highlighted
 	$string = preg_replace('/\$([^$]*)\$/s', '<span class="md-highlight">\1</span>', $string);
+	// Hyperlink
 	$string = preg_replace('/\[(.+)\]\((.+)\)/s', '<a href="\1">\2</a>', $string);
+	// Paragraphs
 	$string = preg_replace('/\+\+J (.+)/', '<p class="justif">\1</p>', $string);
 	$string = preg_replace('/\+\+G (.+)/', '<p class="txt-gauche">\1</p>', $string);
 	$string = preg_replace('/\+\+D (.+)/', '<p class="txt-droite">\1</p>', $string);
 	$string = preg_replace('/\+\+ (.+)/', '<p>\1</p>', $string);
+	// Line break
 	$string = preg_replace('/\/\//', '<br>', $string);
-	$string = preg_replace('/\n\n/', '<br>', $string);
-	$string = preg_replace('/<R>(.*)<\/R>/', '<div class="row">\1</div>', $string);
-	$string = preg_replace('/<C-(\d)>(.*)<\/C>/', '<div class="col-sm-\1">\2</div>', $string);
+	$string = preg_replace('/(\r?\n){2}/', '<br>', $string);
+	// Rows and columns
+	$string = preg_replace('/<R>/s', '<div class="row">', $string);
+	$string = preg_replace('/<\/R>/s', '</div>', $string);
+	$string = preg_replace('/<C-(\d)>/', '<div class="col-sm-\1">', $string);
+	$string = preg_replace('/<\/C>/s', '</div>', $string);
 
 	return $string;
 }
-
-
-
-
-
 
  ?>
