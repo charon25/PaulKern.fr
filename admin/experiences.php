be2ec23840f30482d545523d21a76d32de44d7c8
charon25 <paul.kern@vox.care>
2021-05-05 16:04:40 +0200

    Modification of experience pro

--- admin/experiences.php
+++ admin/experiences.php
@@ -9,16 +9,7 @@ require('../utils/functions.php');
 
 <?php 
 
-$experiences_request = $bdd->query('SELECT * FROM `pk_data` WHERE `type`="pro" OR `type`="extra" ORDER BY `sorter` DESC');
-$experiences_data = array();
-while ($experience = $experiences_request->fetch()) {
-	$experiences_data[] = $experience;
-}
-
- ?>
-
-<?php 
-
+$last_modified = -1;
 if (isset($_POST[$EXP_A_SUBMIT])) {
 	$experience_data = array(
 		$EXP_A_DATE_TXT => $_POST[$EXP_A_DATE_TXT],
@@ -32,11 +23,6 @@ if (isset($_POST[$EXP_A_SUBMIT])) {
 		$photo_path = save_file_random_name($EXP_A_MAIN_IMG, "../", "img/experiences/");
 		$experience_data[$EXP_A_MAIN_IMG] = $photo_path;
 
-		if (count($_FILES[$EXP_A_SEC_IMG]['size']) > 0 && $_FILES[$EXP_A_SEC_IMG]['size'][0] > 0) {
-			$photo_paths = save_multiple_files_random_name($EXP_A_SEC_IMG, "../", "img/experiences/");
-			$experience_data[$EXP_A_SEC_IMG] = $photo_paths;
-		}
-
 		$insert_request = $bdd->prepare('INSERT INTO `pk_data`(`type`, `sorter`, `data`) VALUES (?, ?, ?)');
 		$insert_request->execute(array(
 			$_POST[$EXP_A_TYPE],
@@ -49,7 +35,39 @@ if (isset($_POST[$EXP_A_SUBMIT])) {
 		$neg_result = 'Aucune image principale fournie !';
 	}
 } elseif (isset($_POST[$EXP_A_MODIFY])) {
-	
+	$experience_id = $_POST[$EXP_A_ACTION];
+	$request_experience = $bdd->prepare('SELECT * FROM `pk_data` WHERE id=?');
+	$request_experience->execute(array($experience_id));
+	$experience_data = json_decode($request_experience->fetch()['data'], TRUE);
+
+	set_data_post($experience_data, $EXP_A_DATE_TXT);
+	set_data_post($experience_data, $EXP_A_NAME);
+	set_data_post($experience_data, $EXP_A_TITLE);
+	set_data_post($experience_data, $EXP_A_DESC);
+	set_data_post($experience_data, $EXP_A_SHORT_DESC);
+
+	if (!empty($_FILES) && $_FILES[$EXP_A_MAIN_IMG]['size'] > 0) {
+		$previous_image_path = $experience_data[$EXP_A_MAIN_IMG];
+		unlink("../" . $previous_image_path);
+		$photo_path = save_file_random_name($EXP_A_MAIN_IMG, "../", "img/experiences/");
+		$experience_data[$EXP_A_MAIN_IMG] = $photo_path;
+	}
+
+
+	$insert_request = $bdd->prepare('UPDATE `pk_data` SET `data`=? WHERE `id`=?');
+	$insert_request->execute(array(json_encode($experience_data), $experience_id));
+	$pos_result = "Expérience correctement modifiée !";
+	$last_modified = $experience_id;
+}
+
+ ?>
+
+<?php 
+
+$experiences_request = $bdd->query('SELECT * FROM `pk_data` WHERE `type`="pro" OR `type`="extra" ORDER BY `sorter` DESC');
+$experiences_data = array();
+while ($experience = $experiences_request->fetch()) {
+	$experiences_data[] = $experience;
 }
 
  ?>
@@ -60,7 +78,7 @@ if (isset($_POST[$EXP_A_SUBMIT])) {
 <body>
 
 
-<?php include('../utils/header.php'); print_header(array('start_dir' => "../")); ?>
+<?php //include('../utils/header.php'); print_header(array('start_dir' => "../")); ?>
 
 <section id="presentation-first" class="first-section">
 	<div class="container">
@@ -80,12 +98,13 @@ if (isset($_POST[$EXP_A_SUBMIT])) {
 			<div class="col-sm-3"></div>
 			<div class="col-sm-6">
 				<p class="admin-categorie">Action</p>
-				<p><select class="form-control" id="<?php echo $EXP_A_ACTION; ?>" onchange="on_action_changed();">
-					<option value="X" selected="true">---</option>
+				<form enctype="multipart/form-data" method="post" action="#add-experience" class="form-group">
+				<p><select class="form-control" id="<?php echo $EXP_A_ACTION; ?>" name="<?php echo $EXP_A_ACTION; ?>" onchange="on_action_changed();">
+					<option value="X" <?php if ($last_modified < 0) echo 'selected="true"' ?>>---</option>
 					<?php 
 						foreach ($experiences_data as $key => $experience) {
 							$exp_json = json_decode($experience['data'], TRUE);
-							echo '<option value="' . $experience['id'] . '">' . $exp_json[$EXP_A_NAME] . ' - ' . $exp_json[$EXP_A_TITLE] . '</option>';
+							echo '<option value="' . $experience['id'] . '" ' . ($last_modified == $experience['id'] ? 'selected="true"' : '') . '>' . $exp_json[$EXP_A_NAME] . ' - ' . $exp_json[$EXP_A_TITLE] . '</option>';
 						}
 					 ?>
 				</select></p>
@@ -97,7 +116,6 @@ if (isset($_POST[$EXP_A_SUBMIT])) {
 			</div>
 		</div>
 		<p class="admin-title bleu-big">Ajouter une expérience</p>
-		<form enctype="multipart/form-data" method="post" action="#add-experience" class="form-group">
 			<div class="row">
 				<p class="admin-categorie">Informations générales</p>
 				<div class="col-sm-6 bordure-right-no-padding">
@@ -112,7 +130,7 @@ if (isset($_POST[$EXP_A_SUBMIT])) {
 			<div class="row">
 				<div class="col-sm-6 bordure-right-no-padding">
 					<p class="admin-categorie">Description longue</p>
-					<textarea class="form-control" rows="15" name="<?php echo $EXP_A_DESC; ?>"></textarea>
+					<textarea class="form-control" rows="12" name="<?php echo $EXP_A_DESC; ?>"></textarea>
 				</div>
 				<div class="col-sm-6">
 					<p class="admin-categorie">Description courte</p>
@@ -125,7 +143,6 @@ if (isset($_POST[$EXP_A_SUBMIT])) {
 					</select></p>
 					<p class="admin-categorie">Images</p>
 					<p class="fw-bold">Image principale : <input type="file" name="<?php echo $EXP_A_MAIN_IMG; ?>" class="form-control" accept="image/*"></p>
-					<p class="fw-bold">Images secondaires : <input type="file" name="<?php echo $EXP_A_SEC_IMG . '[]'; ?>" class="form-control" accept="image/*" multiple></p>
 				</div>
 			</div>
 			<p class="text-center margetop15 margebot0">
