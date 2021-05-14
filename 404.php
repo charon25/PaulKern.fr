98fac8c8171458a7d56a551d02e39fe67797202a
Paul Kern <paul.kern@insa-strasbourg.fr>
2021-05-14 11:36:14 +0200

    404 error page

---
+++ 404.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<?php include('utils/head.php'); print_head(array('title' => "Erreur 404")); ?>
+<body>
+
+<?php include('utils/header.php'); print_header(); ?>
+
+<section id="presentation-first" class="first-section">
+	<div class="container">
+		<div class="row">
+			<div class="col-sm-12 text-center">
+				<h1 class="margetop45 fw-bold titre-principal">Erreur 404 : page inconnue</h1>
+				<div class="margebot45"></div>
+			</div>
+		</div>
+	</div>
+</section>
+
+<?php include('utils/footer.php'); print_footer(); ?>
+</body>
+</html>
+
+<script type="text/javascript" src="js/no_js.js"></script>
+<script type="text/javascript" src="js/footer.js"></script>
