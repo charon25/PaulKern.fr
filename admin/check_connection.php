0944b04a7719eb9550830a0976de10a8ad6a6ce4
Paul Kern <paul.kern@insa-strasbourg.fr>
2021-05-26 23:08:56 +0200

    Better protection against non admin user

--- admin/check_connection.php
+++ admin/check_connection.php
@@ -6,7 +6,7 @@
 	$is_session_expired = !isset($_SESSION['activity_time']) || (time() - $_SESSION['activity_time']) > 30 * 60;
 
 	if ($is_not_logged_in || $is_session_expired) {
-		require('../errors/403.php');
+		header('Location: https://www.paulkern.fr/errors/404.php');
 		$is_authentified = FALSE;
 		$_SESSION['login'] = 'false';
 		$_SESSION['activity_time'] = 0;
--- admin/connection.php
+++ admin/connection.php
@@ -19,6 +19,7 @@ if (isset($_POST['connect'])) {
 		if (isset($_COOKIE['visited']) && $_COOKIE['visited'] != 'admin') {
 			$delete_request = $bdd->prepare('DELETE FROM `pk_visits` WHERE `value`=?');
 			$delete_request->execute(array($_COOKIE['visited']));
+			setcookie('visited', 'admin', time() + 3600, null, null, false, true);
 		}
 	} else {
 		$bad_password = TRUE;
